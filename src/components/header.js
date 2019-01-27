e49a4efa89137f95b096a77d303af5fdc6ce9c8d
Egor Kirpichev <egor.kirpichev@gmail.com>
2019-01-27 19:11:38 +0100

    refactor to use styled components

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,37 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
+import styled from "styled-components"
+
+const HeaderWrapper = styled.div({
+  background: 'rebeccapurple',
+  marginBottom: '1.45rem'
+})
+
+const HeaderContainer = styled.div({
+  margin: '0 auto',
+  maxWidth: 960,
+  padding: '1.45rem 1.0875rem'
+})
+
+const H1 = styled.h1({
+  margin: 0,
+  a: {
+    color: 'white',
+    textDecoration: 'none'
+  }
+})
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+  <HeaderWrapper>
+    <HeaderContainer>
+      <H1>
+        <Link to="/">
           {siteTitle}
         </Link>
-      </h1>
-    </div>
-  </div>
+      </H1>
+    </HeaderContainer>
+  </HeaderWrapper>
 )
 
 Header.propTypes = {
